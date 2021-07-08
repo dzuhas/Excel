a511696c6f53a59c8f02afe75e3810b7183ed66e
unknown <Kondratowicz1983@gmail.com>
2021-07-08 23:41:49 +0200

    new fetch

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const uuid = require('uuid').v4;
 const multer = require('multer');
-const xlsx = require('xlsx');
-xlsxj = require("xlsx-to-json");
-const fs = require("fs");
-
+//var upload = multer({ dest: 'upload/' })
+const uuid = require('uuid')
 const path= require('path');
 const app = express();
+var xlsx = require('xlsx')
 app.listen(3001, ()=> console.log("App is listening..."));
 app.use(express.static(
     path.join(__dirname,'public'),));
@@ -16,17 +14,36 @@ console.log("opijajej");
     //  extended: true
     //})
   //);
+  //proba gita
 app.use(express.json());
+// Okreslenie nazwy zapisywanego pliku
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+      cb(null, 'upload/');
+  },
 
-app.post('/upload',(req, res) =>{
+  // By default, multer removes file extensions so let's add them back
+  filename: function(req, file, cb) {
+      cb(null, file.fieldname);
+  }
+});
+const upload = multer({ storage: storage })
+
+app.post('/upload', upload.single('excel.xlsx'), (req, response)=> {
+
+  console.log(req.file);
+  var wb = xlsx.readFile("./upload/excel.xlsx")
+
+  var sheet_name_list = wb.SheetNames;
+  var victory = xlsx.utils.sheet_to_json(wb.Sheets[sheet_name_list[0]])
+  console.log(victory)
+  const myJSON = JSON.stringify(victory);
+  //console.log(myJSON)
+
+  //var ws = wb.Sheets["SUBTOTAL"]
+  //var dat = xlsx.utils.sheet_to_json(ws)
+  return response.json(myJSON);
   
-  console.log("ejze");
-  console.log(req.body);
-
-
-    var name = req.body;
-    console.log(name);
-    
-
-return res.json({status:"akuku"+req.body})
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
 });
