b682dc542c8b2668fd56dde6fb6760a880ec2127
unknown <Kondratowicz1983@gmail.com>
2021-07-20 22:49:23 +0200

    changing key names

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const multer = require('multer');
 const uuid = require('uuid')
 const path = require('path');
 const app = express();
-var xlsx = require('xlsx')
+var xlsx = require('xlsx');
+const Trim = require('trim');
 app.listen(3001, () => console.log("App is listening..."));
 app.use(express.static(
   path.join(__dirname, 'public')));
@@ -35,18 +36,87 @@ app.post('/upload', upload.single('excel.xlsx'), (req, response) => {
   var wb = xlsx.readFile("./upload/excel.xlsx")
 
   var sheet_name_list = wb.SheetNames;
-  var victory = xlsx.utils.sheet_to_json(wb.Sheets[sheet_name_list[0]])
+  console.log(sheet_name_list)
+  const victory = []
+  sheet_name_list.forEach((sheetName) => {
+    victory.push(xlsx.utils.sheet_to_json(wb.Sheets[sheetName]))
+  })
+  //var victory = xlsx.utils.sheet_to_json(wb.Sheets[sheet_name_list[0]])
   console.log(victory)
   function renameKey(obj, oldKey, newKey) {
     obj[newKey] = obj[oldKey];
     delete obj[oldKey];
   }
   //const arr = JSON.parse(victory);
-  victory.forEach(obj => renameKey(obj, 'Imię', 'name'));
-  victory.forEach(obj => renameKey(obj, 'Nazwisko', 'lastname'));
-  victory.forEach(obj => renameKey(obj, 'Adres e-mail', 'email'));
-  victory.forEach(obj => renameKey(obj, 'RegId', 'password'));
-  victory.forEach(obj => renameKey(obj, 'Firma', 'company'));
+  victory.forEach((e) => {
+    e.forEach((b) => {
+
+      if ('Imię' in b) {
+        renameKey(b, 'Imię', 'name')
+      }
+
+      if ('Nazwisko' in b) {
+        renameKey(b, 'Nazwisko', 'lastname')
+      }
+
+      if ('Adres e-mail' in b) {
+        renameKey(b, 'Adres e-mail', 'email')
+      }
+
+      if ('RegId' in b) {
+        renameKey(b, 'RegId', 'password')
+      }
+
+      if ('Firma' in b) {
+        renameKey(b, 'Firma', 'company')
+      }
+
+      if ('Hasło' in b) {
+        renameKey(b, 'Hasło', 'password')
+      }
+
+      if ('Hasło' in b) {
+        renameKey(b, 'Hasło', 'password')
+      }
+
+      if ('Imię i nazwisko' in b) {
+       
+        renameKey(b, 'Imię i nazwisko', 'beforeSplit')
+        const nameLastname = b.beforeSplit 
+        const afterSplit = nameLastname.split(' ');
+        const resultSplit =  afterSplit.filter(e =>  e);
+       // b.name = resultSplit[0]
+        const firstName = resultSplit.shift()
+        b.name = firstName
+        b.lastname = resultSplit.join()
+        delete b.beforeSplit;
+       
+
+    }
+    })
+      
+    console.log(victory)
+
+  })
+  const newVictory = victory.map(sheet => {
+    return sheet.map(pojedynczyUser => {
+      try {
+        pojedynczyUser.password.trim()
+      }  catch(e){
+        console.log("blad gupi2")
+      }
+      try {
+        pojedynczyUser.email.trim()
+      }  catch(e){
+        console.log("blad gupi")
+      }
+      
+      //tutaj robisz jeszcze jakieś inne rzeczy na zmiennej pojedynczyUser
+      return pojedynczyUser // Musisz to zwrócić żeby te operacje się gdzieś zapisały
+    })
+  });
+  console.log(newVictory);
+
 
   const updatedJson = JSON.stringify(victory);
 
